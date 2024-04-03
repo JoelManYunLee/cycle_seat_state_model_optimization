6593b795b2897c7e4cd75894fe55068da66f32f8
andrew44444 <andrewf4t4@gmail.com>
2024-04-03 17:34:57 -0400

    amended activationValues

--- getActivationValues.m
+++ getActivationValues.m
@@ -1,21 +1,16 @@
 function [bfValue, rfValue, gasValue] = getActivationValues( crankAngle )
-    activationValuesMatrix =
-        [ 30, .262, .658, .124
-          60, .379, .477, .283
-          90, .610, .196, .628
-          120, .992, .165, .924
-          150, .639, .114, .725 ]; %second column is bf, third column is rf, fourth column is gas
+    activationValuesMatrix = [ 30 .262 .658 .124; 60 .379 .477 .283; 90 .610 .196 .628; 120 .992 .165 .924; 150 .639 .114 .725 ]; %second column is bf, third column is rf, fourth column is gas
     
-    int i = 1;
+    i = 1;
     if crankAngle == 30
         i = 1;
-    else if crankAngle == 60
+    elseif crankAngle == 60
         i = 2;
-    else if crankAngle == 90
+    elseif crankAngle == 90
         i = 3;
-    else if crankAngle == 120
+    elseif crankAngle == 120
         i = 4;
-    else if crankAngle == 150
+    elseif crankAngle == 150
         i = 5;
     else    
         error('Not a valid crank angle')
