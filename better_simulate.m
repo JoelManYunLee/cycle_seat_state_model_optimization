17baea45978d501b989975a7075ed68f3383edb3
andrew44444 <andrewf4t4@gmail.com>
2024-04-04 23:06:46 -0400

    playing around with function

--- better_simulate.m
+++ better_simulate.m
@@ -16,11 +16,11 @@ function [Vcc] = better_simulate(f0M, resting_length_muscle, resting_length_tend
             a = alpha;
             
             % Modifying properties
-            muscle.resting_length_muscle = resting_length_muscle;
-            muscle.resting_length_tendon = resting_length_tendon;
+            muscle.resting_length_muscle = resting_length_muscle/1000;
+            muscle.resting_length_tendon = resting_length_tendon/1000;
             muscle.f0M = f0M;
     
-            muscle_tendon_length = resting_length_muscle + resting_length_tendon;
+            muscle_tendon_length = resting_length_muscle/1000 + resting_length_tendon/1000;
     
             % Calculate normalized tendon length using HillTypeMuscle method
             norm_lt = muscle.norm_tendon_length(muscle_tendon_length, norm_lm)
