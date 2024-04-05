81dd24f2f6ba0ea34780c0dad1c47be05102c6ea
gavindepiero <gavindepiero7@gmail.com>
2024-04-05 15:04:24 -0400

    zidane

--- zidane_headbutt.m
+++ zidane_headbutt.m
@@ -9,3 +9,11 @@ for i = 1:360
     crank_angles_matrix(i,1) = i/540; % Time
     crank_angles_matrix(i,2) = time_to_crank_angle(i/540); % Crank angle
 end
+
+numAngles = length(crank_angles_matrix);
+hipAngles = zeros(numAngles, 1);
+kneeAngles = zeros(numAngles, 1);
+
+for i = 1:numAngles
+    [hipAngles(i), kneeAngles(i)] = selectJointAngles(crankAngleMatrix(i));
+end
