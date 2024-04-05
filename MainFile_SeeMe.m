e7af4371eb463aaae3245294ece4e8bc99c98770
gavindepiero <gavindepiero7@gmail.com>
2024-04-04 21:52:04 -0400

    adding global variables

--- MainFile_SeeMe.m
+++ MainFile_SeeMe.m
@@ -2,6 +2,10 @@
 clear
 clc
 
+global force_length_regression force_velocity_regression
+force_length_regression = get_muscle_force_length_regression(); 
+force_velocity_regression = get_muscle_force_velocity_regression();
+
 crankAngleMatrix = [30; 60; 90; 120; 150];
 numAngles = length(crankAngleMatrix);
 hipAngles = zeros(numAngles, 1);
