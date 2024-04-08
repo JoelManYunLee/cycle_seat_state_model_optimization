9fc824bd44f4f76ab1213ea6f00a7f6e1ea9d53e
gavindepiero <gavindepiero7@gmail.com>
2024-04-07 20:37:43 -0400

    Using interpolate for vmax now instead of polyfit

--- get_max_shortening_velocity.m
+++ get_max_shortening_velocity.m
@@ -10,10 +10,7 @@ data = [1, 1
     0.4, 0.707
     0.2, 0.68]; % Data taken from "The maximum shortening velocity of muscle shouldbe scaled with activation" John Chow
 
-activation = data(:,1); % Activation data
-max_velo = data(:,2); % Max velocity data
+    F = griddedInterpolant(data(:,1), data(:,2)); % Interpolate data
 
-regression_eqn = polyfit(activation, max_velo, 2); % fit a second order polynomial to the data
-
-max_velocity = regression_eqn(1)*(alpha)^2 + regression_eqn(2)*alpha + regression_eqn(3); % Find max_velocity from regression equation
+    max_velocity = F(alpha);
 end
